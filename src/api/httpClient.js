1b4b8ac630d3ac98877f91e8ca9425edeb8f843c
Yena-Yun <cheryleduart@gmail.com>
2023-05-07 22:03:16 +0900

    Chore: HttpClient의 fetch 함수명 변경, storage.get() 메서드 추가

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -4,13 +4,13 @@ export class HttpClient {
     this.tokenRepository = tokenRepository;
   }
 
-  fetch(endpoint, options = {}) {
+  fetchRequest(endpoint, options = {}) {
     return window.fetch(`${this.baseURL}${endpoint}`, {
       ...options,
       headers: {
         ...options.headers,
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.tokenRepository}`
+        Authorization: `Bearer ${this.tokenRepository.get()}`
       }
     });
   }
