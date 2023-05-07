85d044b6f724f41ff676897d175cb55f0d3585d8
Yena-Yun <cheryleduart@gmail.com>
2023-05-07 21:05:28 +0900

    setting: Base Code 셋팅, 불필요한 기본 파일 삭제

---
+++ src/components/Auth.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+function Auth() {
+  const [userInputs, setUserInputs] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = userInputs;
+
+  const saveUserInputs = ({ target }) => {
+    const { name, value } = target;
+
+    setUserInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const signin = () => {};
+  const signup = () => {};
+  const logout = () => {};
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <div>
+        <label>
+          email
+          <input value={email} name='email' onChange={saveUserInputs} />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input value={password} name='password' onChange={saveUserInputs} />
+        </label>
+      </div>
+      <button onClick={signin}>signin</button>
+      <button onClick={signup}>signup</button>
+      <button onClick={logout}>logout</button>
+    </form>
+  );
+}
+
+export default Auth;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const todos = [];
+
+function Todos() {
+  const [newTodo, setNewTodo] = useState('');
+
+  const saveUserInput = (e) => setNewTodo(e.target.value);
+  const createTodo = () => {};
+
+  return (
+    <div>
+      <input value={newTodo} onChange={saveUserInput} />
+      <button onClick={createTodo}>create</button>
+      {todos?.map(({ id, todo }) => (
+        <li key={id}>{todo}</li>
+      ))}
+    </div>
+  );
+}
+
+export default Todos;
