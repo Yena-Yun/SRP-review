a66f6d0b64483e6ed4a3b7f588f5c8603e1f7945
Yena-Yun <cheryleduart@gmail.com>
2023-05-08 16:36:36 +0900

    Feat: AuthContext 추가

---
+++ src/context/AuthContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from 'react';
+
+const AuthContext = createContext(null);
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children, authService }) => {
+  const signin = authService.signin.bind(authService);
+  const signup = authService.signup.bind(authService);
+  const logout = authService.logout.bind(authService);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signin,
+        signup,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
