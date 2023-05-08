bd60a7e7d01387bee60de337a46a98f584d9ac8f
Yena-Yun <cheryleduart@gmail.com>
2023-05-08 16:39:35 +0900

    Feat: 최상단 index.js에서 Provider에 클래스 객체 주입

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { LocalTokenStorage } from './storage/localTokenStorage';
+import { HttpClient } from './api/httpClient';
+import { AuthProvider } from './context/AuthContext';
+import { TodoProvider } from './context/TodoContext';
+import { AuthService } from './service/Auth/AuthService';
+import { TodoService } from './service/Todo/TodoService';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+const localTokenStorage = new LocalTokenStorage();
+const httpClient = new HttpClient(
+  process.env.REACT_APP_BASE_URL,
+  localTokenStorage
+);
+const authService = new AuthService(httpClient, localTokenStorage);
+const todoService = new TodoService(httpClient);
+
+root.render(
+  <AuthProvider authService={authService}>
+    <TodoProvider todoService={todoService}>
+      <App />
+    </TodoProvider>
+  </AuthProvider>
+);
