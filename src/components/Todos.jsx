f948f715fe3476301bb2fa93d4b9d87afd8803b4
Yena-Yun <cheryleduart@gmail.com>
2023-05-08 16:37:43 +0900

    Feat: Todo 컴포넌트에 함수 로직 추가

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,19 +1,38 @@
 import { useState } from 'react';
-
-const todos = [];
+import { useTodo } from '../context/TodoContext';
 
 function Todos() {
   const [newTodo, setNewTodo] = useState('');
+  const { todos, createTodo, updateTodo, deleteTodo } = useTodo();
 
-  const saveUserInput = (e) => setNewTodo(e.target.value);
-  const createTodo = () => {};
+  const saveUserInput = ({ target }) => {
+    setNewTodo(target.value);
+  }
+
+  const handleCreate = () => {
+    if (!newTodo) return;
+    createTodo(newTodo);
+    setNewTodo('');
+  }
+
+  const handleDelete = async (id) => {
+    await deleteTodo(id);
+  }
 
   return (
     <div>
       <input value={newTodo} onChange={saveUserInput} />
-      <button onClick={createTodo}>create</button>
+      <button onClick={handleCreate}>create</button>
       {todos?.map(({ id, todo }) => (
-        <li key={id}>{todo}</li>
+        <li key={id}>
+            <p>{todo}</p>
+          <div>
+              <button>
+                edit
+              </button>
+            <button onClick={() => handleDelete(id)}>delete</button>
+          </div>
+        </li>
       ))}
     </div>
   );
